b415aa8e4c910b29ff56852ad2c3dd8eb63528fd
Murad5455 <118770310+Murad5455@users.noreply.github.com>
2024-02-18 18:29:30 +0400

    Add project files.

---
+++ HotelFinder.Business/Concrete/HotelManager.cs
@@ -0,0 +1,76 @@
+﻿using HotelFinder.Business.Abstract;
+using HotelFinder.DataAccess.Abstract;
+using HotelFinder.DataAccess.Concrete;
+using HotelFinder.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder.Business.Concrete
+{
+    public class HotelManager : IHotelservice
+    {
+        //private IHotelRepository _hotelRepository;
+
+        //public HotelManager(IHotelRepository hotelRepository)
+        //{
+        //    _hotelRepository = hotelRepository;
+        //}
+
+
+
+        /*IBlogDal _blogDal;
+
+        public BlogManager(IBlogDal blogDal)
+        {
+            _blogDal = blogDal;
+        }*/
+
+        ////////////////////////////////////
+
+        private IHotelRepository _hotelRepository;
+
+        public HotelManager()
+        {
+            _hotelRepository = new HotelRepository();
+        }
+
+
+
+
+
+
+        public Hotel CreateHotel(Hotel hotel)
+      {
+          return _hotelRepository.CreateHotel(hotel);
+      }
+
+      public void DeleteHotel(int id)
+      {
+          _hotelRepository.DeleteHotel(id);
+      }
+
+      public List<Hotel> GetAllHotels()
+      {
+
+          return _hotelRepository.GetAllHotels();
+      }
+
+      public Hotel GetHotelById(int id)
+      {
+          if (id > 0)
+          {
+              return _hotelRepository.GetHotelById(id);
+          }
+          throw new Exception("Id can not be less than 1");
+
+      }
+
+      public Hotel UpdateHotel(Hotel hotel)
+      {
+          return _hotelRepository.UpdateHotel(hotel);
+      }
+  }
+}
